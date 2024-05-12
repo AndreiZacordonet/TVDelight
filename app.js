b605689ebb0e697cf2787abee67b9db10d51ea63
Andrei <andrei.zacordonet@student.tuiasi.ro>
2024-05-13 00:28:47 +0300

    quiz result page implemented

--- app.js
+++ app.js
@@ -4,46 +4,85 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 6789;
 
-// directorul 'views' va conține fișierele .ejs (html + js executat la server)
+// using `file system` nodejs module in an asynchronous manner
+const fs = require('fs').promises;
+
+// 'views' folder contains EJS Files (html + js server-side executed)
 app.set('view engine', 'ejs');
 
-// suport pentru layout-uri - implicit fișierul care reprezintă template-ul site-ului este views / layout.ejs
+// support for layouts - default template file is 'views/layout.ejs'
 app.use(expressLayouts);
 
-// directorul 'public' va conține toate resursele accesibile direct de către client (e.g., fișiere css, javascript, imagini)
+// 'public' Folder contains all client-accesible resources (e.g., css, javascript, images , etc.)
 app.use(express.static('public'))
 
-// corpul mesajului poate fi interpretat ca json; datele de la formular se găsesc în format json în req.body
+// message body can be translated as JSON; JSON Form data is in `req.body`
 app.use(bodyParser.json());
 
-// utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
+// deep parsing algorithm that supports nested objects
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
-// proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
-// proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
-// app.get('/', (req, res) => res.render('layout', req.query));
+// Request object properties - req - https://expressjs.com/en/api.html#req
+// Response object properties - res - https://expressjs.com/en/api.html#res
+// returns hello world when accesing http://localhost:6789/
 app.get('/', (req, res) => res.send('Hello World'));
+// app.get('/', (req, res) => res.render('layout', req.query));
 
-// la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
+// called when accesing http://localhost:6789/chestionar 
 app.get('/chestionar', (req, res) => {
-    const quiz = [
-        {
-            question: 'Question 1',
-            options: ['option 1', 'option 2', 'option 3', 'option 4'],
-            correct: 0
-        },
-        //...
-    ];
-// în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care
-// conține vectorul de întrebări
-    res.render('chestionar', { quiz: quiz });
-    // res.render('layout', { body: 'chestionar', questionList: quiz });
+    // reads the quiz from file in an syncronous manner 
+    // const quiz = require('intrebari.json');
+
+    fs.readFile('intrebari.json')
+        // `.then` method is executed only when the file reading is successful
+        .then(data => {
+            // parsing JSON data
+            const quiz = JSON.parse(data);
+            // render the 'chestionar' view with the quiz data
+            res.render('chestionar', { quiz });
+        })
+        .catch(error => {
+            // handle any errors
+            console.error('Error reading or parsing file:', error);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 app.post('/rezultat-chestionar', (req, res) => {
-    console.log(req.body);
-    res.send("formular: " + JSON.stringify(req.body));
+    // res.send("formular: " + JSON.stringify(req.body));
+
+    fs.readFile('intrebari.json')
+        // `.then` method is executed only when the file reading is successful
+        .then(data => {
+            // parsing JSON data
+            const quiz = JSON.parse(data);
+
+            // taking the post request from the Quiz Form
+            const submittedAnswers = req.body;
+            let correctAns = 0;
+
+            // parsing the object from request
+            Object.keys(submittedAnswers).forEach((questionKey) => {
+                const questionIndex = parseInt(questionKey.slice(1));
+
+                const submittedAnswerIndex = parseInt(submittedAnswers[questionKey]);
+
+                // every question has a correct option
+                const correct = quiz[questionIndex].correct;
+
+                if (submittedAnswerIndex == correct) {
+                    correctAns++;
+                }
+            });
+
+            // seding the number of correct anwers to the Result File
+            res.render('rezultat-chestionar', { correctAns });
+        })
+        .catch(error => {
+            // handle any errors
+            console.error('Error reading or parsing file:', error);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
-app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
+app.listen(port, () => console.log('Server runing at http://localhost:' + port));
